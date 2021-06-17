5cd57aeda97b3a79b916fb19452ee89355dc2d64
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-17 15:21:33 +0200

    Model

---
+++ src/main/java/com/zekerijah/doggraphqlapi/model/Dog.java
@@ -0,0 +1,22 @@
+package com.zekerijah.doggraphqlapi.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String breed;
+    private String origin;
+}
