4a9c8685fafd0efa4c8e8bf795936872fbeaeec7
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-17 15:52:02 +0200

    Resolver

---
+++ src/main/java/com/zekerijah/doggraphqlapi/resolver/Query.java
@@ -0,0 +1,34 @@
+package com.zekerijah.doggraphqlapi.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.zekerijah.doggraphqlapi.exception.DogNotFoundException;
+import com.zekerijah.doggraphqlapi.model.Dog;
+import com.zekerijah.doggraphqlapi.repository.DogRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+    private DogRepository dogRepository;
+
+    public Query(DogRepository dogRepository) {
+        this.dogRepository = dogRepository;
+    }
+
+    public Iterable<Dog> findAllDogs() {
+        return dogRepository.findAll();
+    }
+
+    public Dog findDogById (Long id){
+        Optional<Dog> optionalDog = dogRepository.findById(id);
+
+        if(optionalDog.isPresent()) {
+            return optionalDog.get();
+        } else {
+            throw new DogNotFoundException("Dog not found", id);
+        }
+    }
+
+
+}
